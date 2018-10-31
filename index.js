a9cb06ee265ffd84d5df2f131bdebe8e42aaddff
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-31 13:34:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node').querySelector('div').querySelector('div').querySelector('div').querySelector('div')
+}
+
+function increaseRankBy(n){
+  const lists = document.querySelectorAll('.ranked-list')
+   for(let i = 0; i < lists.length; i++){
+    let children = lists[i].children;
+     for(let j = 0; j < children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
