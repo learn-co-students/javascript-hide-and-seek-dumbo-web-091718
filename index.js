d2a28b19a3f38fb74187f82ed878767192ad8d58
github username <github email address>
2018-10-31 11:49:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector);
+}
 
+function nestedTarget(){
+    return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+    let rankedList = document.querySelectorAll('.ranked-list li')
+    rankedList.forEach(function(li) {
+        li.innerText = parseInt(li.innerText) + n
+    })
+}
+
+function deepestChild(){
+    return document.querySelector('#grand-node div div div div')
+}
