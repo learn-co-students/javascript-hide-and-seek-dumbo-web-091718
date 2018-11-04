265459e7d732613704700ae0b9559406d3cebe0d
tiffanynuugen <tiffanynuugen@gmail.com>
2018-11-03 20:13:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
